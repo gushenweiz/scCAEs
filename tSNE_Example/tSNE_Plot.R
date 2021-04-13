bec7695db7a78ce281a76eb380cf3634f3357fa1
HH <53171345+gushenweiz@users.noreply.github.com>
2021-04-13 15:35:38 +0800

    Update tSNE_Plot.R

--- tSNE_Example/tSNE_Plot.R
+++ tSNE_Example/tSNE_Plot.R
@@ -17,7 +17,28 @@ class.label<- class.label[,2]
 cluster=class.label
 cluster<-as.factor(cluster)
 
+
+
 p<-ggplot(data=tsnepc1, aes(x=var_x, y=var_y,color=cluster)) + geom_point(size=1)+
+  #labs(title="full", x="TSNE1", y="TSNE2")+
+  theme(plot.title = element_text(hjust = 0.5))+theme_classic()
+#p<-p+theme(legend.position="none")
+p<-p+labs(x =NULL,y = NULL)
+p<-p+labs(x ="tSNE_1",y = "tSNE_2")
+p<-p+theme(legend.position="none")
+p<-p+theme(axis.text=element_text(size=10),axis.title = element_text(face = "bold")) + theme(legend.title = element_blank())
+
+p
+
+
+class.label<- read.table("label_Zeisel.csv", header=T,sep=",",check.names=F) 
+class.label<-as.matrix(class.label)
+class.label<- class.label[,2]
+label=class.label
+label<-as.factor(label)
+
+
+p<-ggplot(data=tsnepc1, aes(x=var_x, y=var_y,color=label)) + geom_point(size=1)+
   #labs(title="full", x="TSNE1", y="TSNE2")+
   theme(plot.title = element_text(hjust = 0.5))+theme_classic()
 #p<-p+theme(legend.position="none")
